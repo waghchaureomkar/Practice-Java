498bf87002fd54cd5cea06d0cdda0e8d90e9e4b3
waghchaureomkar <waghchaureomkar@localhost>
2023-11-18 18:40:43 +0530

    pattern1 don

--- src/practice/pattern/PT01_Star.java
+++ src/practice/pattern/PT01_Star.java
@@ -1,5 +1,34 @@
 package practice.pattern;
 
 public class PT01_Star {
+	public static void main(String[] args) {
 
+		PatternsStar.pattern1(5);
+
+	}
+
+}
+
+class PatternsStar {
+
+//	patterns 1--------------
+
+//	* * * * *
+//	* * * * *
+//	* * * * *
+//	* * * * *
+//	* * * * *
+	public static void pattern1(int n) {
+		System.out.println("1.pattern -------------->\n");
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < n; j++) {
+				System.out.print("* ");
+
+			}
+			System.out.println(" ");
+		}
+
+	}
 }
