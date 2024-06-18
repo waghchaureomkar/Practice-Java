4a67e0b42a2a4d70f3c5d3741740e7e864febed1
waghchaureomkar <waghchaureomkar@gmail.com>
2024-06-18 22:06:44 +0530

    added problem

---
+++ src/practice/dataStructure/queue/QueueArray.java
@@ -0,0 +1,22 @@
+package practice.dataStructure.queue;
+
+public class QueueArray {
+
+	static class Queue {
+		static int arr[];
+		static int size;
+		static int rear = -1;
+
+		Queue(int n) {
+			arr = new int[n];
+			this.size = n;
+		}
+		
+
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("");
+	}
+}
---
+++ src/practice/problems/FirstNonDublicate.java
@@ -0,0 +1,28 @@
+package practice.problems;
+
+import java.util.HashMap;
+
+public class FirstNonDublicate {
+	
+	static void Omkar(String key,Integer value) {
+		System.out.println(key+value);
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "swis";
+		
+		HashMap<String,Integer> arr = new HashMap<>();
+		arr.put("Omkar", 8);
+		arr.put("vi", 2);
+		arr.put("Omjdkar", 8);
+		arr.put("diw", 8);
+
+		
+		arr.forEach((om,value)->{
+			System.out.println(om+value);
+		});
+		
+	}
+
+}
